a753c731a8461aedff43716b34fac277c9a23cb5
Karthi788 <karthisenthilkumar789@gmail.com>
2025-05-14 14:05:58 +0530

    ClassandObjects

---
+++ ClassandObjects.java
@@ -0,0 +1,8 @@
+public class ClassandObjects{
+    int x = 5;
+
+    public static void main(String[] args) {
+        ClassandObjects obj = new ClassandObjects();
+        System.out.println(obj.x);
+    }
+}
